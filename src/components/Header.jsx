1b21d4ca0cd29130e80c432ae7051acb6b59617b
God'swill Sekav Apya <godswillapya@gmail.com>
2024-12-31 16:31:09 +0100

    Constants Folder Upload and Header Build

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { brainwave } from "../assets";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm">
+      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+        <a href="#hero" className="block w-[12rem] xl:mr-8">
+          <img src={brainwave} width={190} height={40} alt="Brainwave" />
+        </a>
 
-export default Header
+        <nav className="hidden fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent">
+          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row"></div>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
