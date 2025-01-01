c528003f2d7ef4d7ebac1846751fc808f0cdb745
God'swill Sekav Apya <godswillapya@gmail.com>
2025-01-01 14:56:29 +0100

    Section Component Commencement

---
+++ src/components/Sections.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sections = ({className, id, crosses, crossesOffset, customPaddings, children}) => {
+  return (
+    <div>Sections</div>
+  )
+}
+
+export default Sections
