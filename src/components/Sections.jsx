cd919783c4a9c403ab724d4e511120ce2ff1fa77
God'swill Sekav Apya <godswillapya@gmail.com>
2025-01-02 18:00:22 +0100

    Sections Upload

--- src/components/Sections.jsx
+++ src/components/Sections.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Sections = ({className, id, crosses, crossesOffset, customPaddings, children}) => {
   return (
-    <div>Sections</div>
+    <div className={`relative ${customPaddings || `py-10 lg:py-16 xl:py-20 ${crosses ? 'lg:py-32 xl:py-40' : ''}`}`}>Sections</div>
   )
 }
 
